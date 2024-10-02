c48d4bef7e50ac4da80c933e3db73bc3926af75a
asifalam515 <asibulalam515@gmail.com>
2024-10-02 08:20:45 +0600

    github and google log in applied

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,64 @@
 import { useContext } from "react";
 import { AuthContext } from "../providers/AuthProviders";
+import { FaGoogle } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+
 import Swal from "sweetalert2";
+import {
+  getAuth,
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+import app from "../firebase/firebase.config";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const { loggedInUser } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const auth = getAuth(app);
+
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
+  const handleGoogleLogin = () => {
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        // logge in success
+        Swal.fire({
+          icon: "success",
+          title: "Logged In using Google",
+          text: "Logged In Successfully",
+        });
+        navigate("/");
+      })
+      .catch((error) => {
+        Swal.fire({
+          icon: "error",
+          title: "Something went Wrong",
+          text: `${error.message}`,
+        });
+      });
+  };
+  const handleGithubLogin = () => {
+    signInWithPopup(auth, githubProvider)
+      .then((result) => {
+        // logge in success
+        Swal.fire({
+          icon: "success",
+          title: "Logged In using Github",
+          text: "Logged In Successfully",
+        });
+        navigate("/");
+      })
+      .catch((error) => {
+        Swal.fire({
+          icon: "error",
+          title: "Something went Wrong",
+          text: `${error.message}`,
+        });
+      });
+  };
+
   const handleLogIn = (e) => {
     e.preventDefault();
     const form = e.target;
@@ -19,20 +74,18 @@ const Login = () => {
         title: "Logged In",
         text: "Logged In Successfully",
       });
+      navigate("/");
     });
   };
   return (
     <div>
       <div className="hero bg-base-200 min-h-screen">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="text-center lg:text-left">
+        <div className="hero-content grid grid-rows-2">
+          {/* text div */}
+          <div className="text-center ">
             <h1 className="text-5xl font-bold">Login now!</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
           </div>
+          {/* form div */}
           <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
             <form onSubmit={handleLogIn} className="card-body">
               <div className="form-control">
@@ -63,6 +116,17 @@ const Login = () => {
                 <button className="btn btn-primary">Login</button>
               </div>
             </form>
+            {/* 
+            login Using github and google
+            */}
+            <div className="flex justify-center items-center gap-10">
+              <button onClick={handleGoogleLogin} className="btn btn-sm">
+                <FaGoogle />
+              </button>
+              <button onClick={handleGithubLogin} className="btn btn-sm">
+                <FaGithub />
+              </button>
+            </div>
           </div>
         </div>
       </div>
