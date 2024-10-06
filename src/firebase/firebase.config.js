95e4db4395fe79ce78007b659f36a1041c86bd74
asifalam515 <asibulalam515@gmail.com>
2024-10-06 10:02:09 +0600

    firebase configuration setted hidden

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -3,14 +3,17 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyABXZs6zcjTfvM7p5-YZl9fLRT7O1kcto0",
-  authDomain: "tourism-hub-de7b4.firebaseapp.com",
-  projectId: "tourism-hub-de7b4",
-  storageBucket: "tourism-hub-de7b4.appspot.com",
-  messagingSenderId: "270409171434",
-  appId: "1:270409171434:web:3939845ee19b0bc054b293",
+const firebaseConfigTourism = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY_TOURISM,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN_TOURISM,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID_TOURISM,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET_TOURISM,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID_TOURISM,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID_TOURISM,
 };
 
 // Initialize Firebase
